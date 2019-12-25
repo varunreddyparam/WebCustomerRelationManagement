6582795b81fac7e98260930e514d09cb9f087c84
varunreddyparam <varunreddyparam@gmail.com>
2019-12-24 18:19:49 -0800

    ORM Dapper is Added

---
+++ WebCustomerRelationManagement.Concrete/DapperORM.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCustomerRelationManagement.Concrete
+{
+    public static class DapperORM
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["DatabaseConnectionString"].ToString();
+
+        public static void ExecuteWithOutReturn(string proceedureName, DynamicParameters parameters = null)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                con.Execute(proceedureName, parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public static T ExecuteReturnScalar<T>(string proceedureName, DynamicParameters parameters = null)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                return (T)Convert.ChangeType(con.Execute(proceedureName, parameters, commandType: CommandType.StoredProcedure), typeof(T));
+            }
+        }
+
+        public static IEnumerable<T> ReturnList<T>(string proceedureName, DynamicParameters parameters = null)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                return con.Query<T>(proceedureName, parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
---
+++ WebCustomerRelationManagement.Interface/IContacts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCustomerRelationManagement.Interface
+{
+    public interface IContacts 
+    {
+        int GetTotalContactsCount();
+
+        int GetTotalContactbyType(int contactType);
+        
+        
+        
+
+    }
+}
