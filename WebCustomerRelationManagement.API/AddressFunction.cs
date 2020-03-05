d50b4e30a49df9397756bd1542b75cb4dede6a57
varunreddyparam <varunreddyparam@gmail.com>
2020-03-04 23:51:31 -0500

    Azure Functions Added

--- WebCustomerRelationManagement.API/AddressFunction.cs
+++ WebCustomerRelationManagement.API/AddressFunction.cs
@@ -1,12 +1,12 @@
 
-using System.IO;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
-using WebCustomerRelationManagement.Models;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 using WebCustomerRelationManagement.Concrete;
 
 namespace WebCustomerRelationManagement.API
@@ -14,20 +14,24 @@ namespace WebCustomerRelationManagement.API
     public static class AddressFunction
     {
         [FunctionName("AddressFunction")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]HttpRequest req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]HttpRequest req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
-
+            //AddressConcrete addressConcrete = new AddressConcrete(Environment.GetEnvironmentVariable("AccountConnectionStrings"));
             string id = req.Query["id"];
-            AddressConcrete addressConcrete = new AddressConcrete();
-            //string requestBody = new StreamReader(req.Body).ReadToEnd();
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
 
-            //dynamic data = JsonConvert.DeserializeObject(requestBody);
-            id = JsonConvert.SerializeObject(addressConcrete.Get(id);
 
-            return id != null
-                ? (ActionResult)new OkObjectResult(id)
-                : new BadRequestObjectResult("Please pass a id on the query string");
+            return await AddressConcrete.Get(id) != null
+                ? new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+                {
+                    Content = new StringContent(JsonConvert.SerializeObject(await AddressConcrete.Get(id)))
+                }
+                : new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent("Please pass a id on the query string")
+                };
         }
     }
 }
--- WebCustomerRelationManagement.Concrete/AddressConcrete.cs
+++ WebCustomerRelationManagement.Concrete/AddressConcrete.cs
@@ -1,53 +1,60 @@
-﻿using Microsoft.Azure;
-using Microsoft.WindowsAzure.Storage;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Table;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using WebCustomerRelationManagement.Interface;
+using System.Threading.Tasks;
 using WebCustomerRelationManagement.Models;
 
 namespace WebCustomerRelationManagement.Concrete
 {
-    public class AddressConcrete : IAddress
+    public static class AddressConcrete
     {
-        private static string ConnectionStringConfig = "StorageConnectionString";
         private const string key = "Address";
-        public string CreateOrUpdate(AddressEntity addressEntity)
+        public async static Task<string> CreateOrUpdate(AddressEntity addressEntity)
         {
             throw new NotImplementedException();
         }
 
-        public void Delete(string id)
+        public async static void Delete(string id)
         {
             if (Get(id) == null)
             {
                 return;
             }
-            GetTable().Execute(TableOperation.Delete(Get(id)));
+            await GetTable().ExecuteAsync(TableOperation.Delete(await Get(id)));
         }
 
-        public AddressEntity Get(string id)
+        public async static Task<AddressEntity> Get(string id)
         {
-            return GetTable().CreateQuery<AddressEntity>().Where(x => x.PartitionKey == key && x.RowKey == id).SingleOrDefault();
+            //return GetTable().CreateQuery<AddressEntity>().Where(x => x.PartitionKey == key && x.RowKey == id).SingleOrDefault();
+            TableOperation tableOperation = TableOperation.Retrieve(key, id);
+            TableResult tableResult = await GetTable().ExecuteAsync(tableOperation);
+            return tableResult.Result as AddressEntity;
         }
 
-        public List<AddressEntity> GetAll()
+        public async static Task<List<AddressEntity>> GetAll()
         {
             var table = GetTable();
             return table.CreateQuery<AddressEntity>().Where(x => x.PartitionKey == key).ToList();
         }
         private static CloudTable GetTable()
         {
-            // retrieve the storage account details by passing in our 
-            // ConnectionStringConfig constant value
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
-                CloudConfigurationManager.GetSetting(ConnectionStringConfig));
-
-            var client = storageAccount.CreateCloudTableClient();
-            // creates a CloudTable instance pointing to the table for the name
-            // defined in the PK constant.
-            return client.GetTableReference(key);
+            string accountName = "monkeycrm";
+            string accountKey = "FSpn7CPjKKMOEdFggeBpfXZd71ZfeQpKSopP8DLZsIo5np3Y3oUrNN8PC/2bRTwsgFL9Abcy0mtT54ij1305AQ==";
+            try
+            {
+                StorageCredentials creds = new StorageCredentials(accountName, accountKey);
+                CloudStorageAccount account = new CloudStorageAccount(creds, useHttps: true);
+                CloudTableClient client = account.CreateCloudTableClient();
+                CloudTable table = client.GetTableReference(key);
+                return table;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
