6754096c2123992e3afa6dd3537884f8026404d1
varunreddyparam <varunreddyparam@gmail.com>
2020-03-16 23:15:12 -0400

    Query Engine

---
+++ QueryEngine/Query.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace QueryEngine
+{
+    public class Query
+    {
+        TableQuery tableQuery = new TableQuery();
+
+        public void SelectQuery()
+        {
+            tableQuery.
+        }
+        
+    }
+}
