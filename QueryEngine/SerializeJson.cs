d120be11dc1806c94b33a8332b2bc3917bd6c9d9
varunreddyparam <varunreddyparam@gmail.com>
2020-03-17 16:37:21 -0400

    Query Engine

---
+++ QueryEngine/SerializeJson.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryEngine
+{
+    public static class SerializeJson
+    {
+        public static string ToJson(this Query self) => JsonConvert.SerializeObject(self, QueryEngine.Converter.Settings);
+    }
+}
